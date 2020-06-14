bb05c8b98239f1b73d7e8c0add4a78d7bb485eb3
Noforgive <maithaison1998@gmail.com>
2020-06-14 12:16:01 +0700

    do an tong hop

---
+++ nhom17_thuongmaidientu/src/main/java/nhom17/thuongmaidientu/repository/LoaiSanPhamRepository.java
@@ -0,0 +1,17 @@
+package nhom17.thuongmaidientu.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import nhom17.thuongmaidientu.model.LoaiSanPham;
+
+@Repository
+public interface LoaiSanPhamRepository extends JpaRepository<LoaiSanPham, Integer> {
+
+	@Query(value = "select * from loaisanpham",nativeQuery = true)
+	List<LoaiSanPham> TimTatCaLoaiSanPham();
+	
+}
---
+++ nhom17_thuongmaidientu/src/main/java/nhom17/thuongmaidientu/repository/NhaCungCapRepository.java
@@ -0,0 +1,17 @@
+package nhom17.thuongmaidientu.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import nhom17.thuongmaidientu.model.NhaCungCap;
+
+@Repository
+public interface NhaCungCapRepository extends JpaRepository<NhaCungCap, Integer> {
+
+	@Query(value = "select * from nhacungcap",nativeQuery = true)
+	List<NhaCungCap> TimTatCaNhaCungCap();
+	
+}
